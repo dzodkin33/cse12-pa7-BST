577a8f0f3263e371522ffde78635f0c6d571a366
Boris Ryabov <stonks33@gmail.com>
2022-05-17 00:39:18 -0700

    [ADD] Added a series of tests for put
    [ADD] Added a series of tests for put

--- pa7-starter/src/BSTTest.java
+++ pa7-starter/src/BSTTest.java
@@ -8,10 +8,48 @@ import java.util.Random;
 import org.junit.*;
 
 public class BSTTest {
-	
-	/* TODO: Add your own tests */
-	@Test
-	public void dummyTest() {
-		
+
+	BST<Integer, Integer> testTree;
+
+	@Before
+	public void before() {
+		testTree = new BST<Integer, Integer>();
 	}
+	
+	// Testing constructor method
+	// TODO: add header comments
+	@Test
+	public void constructorTest() {
+		assertNotNull(testTree);
+	}
+	
+
+	@Test
+	public void testPut() {
+		int expectedSize = 1;
+		assertTrue(testTree.put(1, 2));
+
+		assertEquals(expectedSize, testTree.size());
+	}
+
+	@Test
+	public void testPutDublicateKeys(){
+		int expectedSize = 1;
+		testTree.put(1, 3);
+
+		assertFalse(testTree.put(1, 2));
+		assertEquals(expectedSize, testTree.size());
+	}
+
+	@Test 
+	public void testPutCorrectPlacement() {
+
+	}
+	// ! Finish testing exception
+	// @Test 
+	// public void testPutNullKey() {
+	// 	String expectedMessage = new IllegalAccessError().getMessage();
+	// 	assertThrows(testTree.put(null, 2).compareTo())
+
+	// }
 }
